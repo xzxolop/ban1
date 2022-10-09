8657825fd7abc18427139ef13d8156ba7edb45fe
Egor Voronin <lavaava44@gmail.com>
2022-10-09 23:02:08 +0300

    кол-во дней между 2 годами

--- DateTime.pas
+++ DateTime.pas
@@ -1,5 +1,12 @@
-﻿begin
-  var i:= readinteger();
-  if ((i mod 4 = 0) and (i mod 100 <> 0)) or (i mod 400 = 0) then writeln('Високосный-366')
-  else writeln('Невисокосный-365');
+﻿begin  
+  var(a,b) := readinteger2();
+  var c:= 0;
+  for var i:=a to b do 
+  begin
+    if ((i mod 4 = 0) and (i mod 100 <> 0)) or (i mod 400 = 0) then 
+      c:= c+366
+  else
+    c:=c+365;
+  end;
+  print(c);
 end.
