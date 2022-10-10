221f417b3b8028f00c9e01f810e0321105ec354f
Egor Voronin <lavaava44@gmail.com>
2022-10-10 20:02:25 +0300

    add functional hours to seconds

--- DateTime.pas
+++ DateTime.pas
@@ -1,12 +1,4 @@
 ﻿begin  
-  var(a,b) := readinteger2();
-  var c:= 0;
-  for var i:=a to b do 
-  begin
-    if ((i mod 4 = 0) and (i mod 100 <> 0)) or (i mod 400 = 0) then 
-      c:= c+366
-  else
-    c:=c+365;
-  end;
-  print(c);
+  var a:= readinteger('Введите колличество часов:');
+  print(a*3600);
 end.
